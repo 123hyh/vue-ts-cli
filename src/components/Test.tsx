ab1668550263eefdffb9b2bfdfc3911ff9ec701d
123hyh <310515005@qq.com>
2020-03-17 00:19:41 +0800

    no message

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,12 +1,17 @@
-import   {ComponentOptions,CreateElement} from 'vue'
-
-export const Test: any = {
+import  vue, {ComponentOptions,CreateElement, VueConstructor} from 'vue'
+export const Test: VueConstructor<any> = vue.extend({
   render(h: any): any{
-    return(<div>{this.name}</div>)
+    return(<div onClick={this.handlerClick}>{this.name}</div>)
+  },
+  methods:{
+    handlerClick(e: MouseEvent): void{
+      e.stopPropagation()
+      console.log(2)
+    }
   },
   props:{
     name: {
       type: String
     }
   }
-}
+})
