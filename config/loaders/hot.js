8fc62c5d9b72f9a2d093f0ae9f906180840f692c
huangyuhui <310515005@qq.com>
2020-04-08 00:09:50 +0800

    完成 热更新 loader 的编写

--- config/loaders/hot.js
+++ config/loaders/hot.js
@@ -1,13 +1,39 @@
-const schema = {
-  type: "object",
-  properties: {
-    test: {
-      type: "string"
-    }
-  }
-};
+
+const hash = require('hash-sum');
 
 module.exports = function(source) {
   // 对资源应用一些转换……
+  let { request, resourcePath } = this;
+  if(/\.tsx$/.test(resourcePath)){
+    const id = hash(request);
+    let path = resourcePath.split(/\\/);
+    path = `./${path[path.length - 1]}`;
+    /* 添加导入文件和导入文件 */
+    source = `
+      import * as scripts from '${path}'; \n
+      export * from '${path}'; \n
+      ${source} \n
+      /* 热更新处理 */   
+      ;if ( module.hot ) {
+
+        const api = require('vue-hot-reload-api');
+        const Vue = require('vue')
+
+        api.install(Vue);
+        module.hot.accept();
+
+        /* 批量处理 */
+        const scriptKeys = Object.keys(scripts);
+        if(module.hot.data){
+          scriptKeys.forEach( (item, i) => {
+            api.rerender('${id}', scripts[item])
+          })
+        }else{
+          scriptKeys.forEach( (item, i) => {
+            api.createRecord('${id}', scripts[item])
+          })
+        }
+    }`;
+  }
   return source;
 };
