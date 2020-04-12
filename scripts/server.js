5de47c26f49946ae6b640fd0077f980ecfe528fb
123hyh <310515005@qq.com>
2020-04-12 11:21:13 +0800

    优化 开发环境

--- scripts/server.js
+++ scripts/server.js
@@ -4,48 +4,68 @@ const Webpack = require('webpack');
 const webpackConfig = require('../config/dev.js');
 const WebpackDevServer = require('webpack-dev-server');
 
-const devServerOptions = {
-  contentBase: path.resolve(process.cwd(), 'dist'),
-  hot: true,
-  quiet: true,
-  inline: true,
-  overlay: {
-    errors: true,
-    warnings: false,
-  },
-  proxy: {
-    '/apis': {
-      target: 'http://47.106.230.157:8080/apis',
-      pathRewrite: { '^/apis': '' },
-      changeOrigin: true,
-    },
-  },
-};
+class Server {
+  constructor() {
+    this.options = {
+      contentBase: path.resolve(process.cwd(), 'dist'),
+      hot: true,
+      quiet: true,
+      clientLogLevel: 'none',
+      inline: true,
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+      proxy: {
+        '/apis': {
+          target: 'http://47.106.230.157:8080/apis',
+          pathRewrite: { '^/apis': '' },
+          changeOrigin: true,
+        },
+      },
+    };
+    this.bootstrapDevService();
+  }
+  /* 启动程序 */
+  bootstrapDevService() {
+    this.handlerPort().then((port) => {
+      const compile = Webpack(webpackConfig(port));
 
-function startService(port = 8080) {
-  var server = net.createServer().listen(port);
+      const devService = new WebpackDevServer(compile, {
+        ...(compile.devServer || {}),
+        ...this.options,
+      });
 
-  server.on('listening', function () {
-    // 执行这块代码说明端口未被占用
-    server.close(); // 关闭服务
-
-    /* 启用 WebpackDevServer */
-    const compile = Webpack(webpackConfig(port));
-    const devService = new WebpackDevServer(compile, {
-      ...(compile.devServer || {}),
-      ...devServerOptions,
+      /* 启用 WebpackDevServer */
+      devService.listen(port, (e) => {
+        console.log(e);
+      });
     });
-    devService.listen(port, (e) => {
-      console.log(e);
-    });
-  });
+  }
 
-  server.on('error', function (err) {
-    if (err.code === 'EADDRINUSE') {
-      // 端口已经被使用
-      startService((port += 1));
-    }
-  });
+  /* 处理 启动端口占用 */
+  handlerPort() {
+    return new Promise((resolve) => {
+      this.selectorStartPort(resolve);
+    });
+  }
+  /* 挑选端口 */
+  selectorStartPort(cb, port = 8080) {
+    var server = net.createServer().listen(port);
+
+    server.on('listening', () => {
+      // 执行这块代码说明端口未被占用
+      server.close(); // 关闭服务
+      cb(port);
+    });
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        // 端口已经被使用
+        this.selectorStartPort((port += 1));
+      }
+    });
+  }
 }
 
-startService();
+new Server();
